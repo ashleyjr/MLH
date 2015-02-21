55d267c07a81645259ad8ba98058b031ebf9930d
unknown <Ashley Robinson@SID-003907.cdp.local>
2015-02-21 14:00:59 +0000

    uart rx to tx loop, not working

--- Implementation/FPGA/top.v
+++ Implementation/FPGA/top.v
@@ -7,17 +7,27 @@ module top(
 
    wire [7:0]     data_rx;
    reg [7:0]      data_tx;  
+   wire           busy_rx;
+
+   wire loop;
+
+   always @(posedge clk or negedge nRst) begin
+      if (!nRst)  data_tx <= 0;
+      else begin
+         data_tx <= data_tx + 1;
+      end
+   end
 
    uart uart(
       .clk        (clk        ),
       .nRst       (nRst       ),
-      .transmit   (1'h0       ),
-      .data_tx    (8'hAA      ),
-      .rx         (rx         ),
-      .busy_rx    ( ),
+      .transmit   (!busy_rx       ),
+      .data_tx    (8'hAA   ),
+      .rx         (loop        ),
+      .busy_rx    ( busy_rx),
       .recieved   (recieved   ),
       .data_rx    (data_rx    ),
-      .tx         (tx         )
+      .tx         (loop       )
    );
 
 endmodule
--- Implementation/FPGA/uart.v
+++ Implementation/FPGA/uart.v
@@ -4,6 +4,7 @@ module uart(
    input          transmit,   // Raise to trasmit
    input    [7:0] data_tx,    // Transmit this
    input          rx,
+   output         busy_tx,
    output         busy_rx,
    output         recieved,   // Raised when byte recieved, zero when getting
    output   [7:0] data_rx,    // This is recieved
@@ -11,6 +12,7 @@ module uart(
 );
 
    // Outputs
+   reg         busy_tx;
    reg         recieved;
    reg         tx;
    reg         busy_rx;
@@ -21,67 +23,133 @@ module uart(
    parameter   BAUD = 9'd434, 
                BAUD_05 = BAUD / 2;
 
-   parameter   IDLE  = 4'b0000, 
-               START = 4'b0001, 
-               RX_1  = 4'b0010,
-               RX_2  = 4'b0011,
-               RX_3  = 4'b0100,
-               RX_4  = 4'b0101,
-               RX_5  = 4'b0110,
-               RX_6  = 4'b0111,
-               RX_7  = 4'b1000,
-               RX_8  = 4'b1001,
-               WAIT  = 4'b1010;
+   parameter   RX_IDLE  = 4'b0000, 
+               RX_START = 4'b0001, 
+               RX_1     = 4'b0010,
+               RX_2     = 4'b0011,
+               RX_3     = 4'b0100,
+               RX_4     = 4'b0101,
+               RX_5     = 4'b0110,
+               RX_6     = 4'b0111,
+               RX_7     = 4'b1000,
+               RX_8     = 4'b1001,
+               RX_WAIT  = 4'b1010;
 
+   parameter   TX_IDLE  = 4'b0000, 
+               TX_START = 4'b0001, 
+               TX_1     = 4'b0010,
+               TX_2     = 4'b0011,
+               TX_3     = 4'b0100,
+               TX_4     = 4'b0101,
+               TX_5     = 4'b0110,
+               TX_6     = 4'b0111,
+               TX_7     = 4'b1000,
+               TX_8     = 4'b1001,
+               TX_WAIT  = 4'b1010;
 
    // Internal Regs
-   reg [3:0]   state;
-   reg [8:0]   count;
+   reg [3:0]   state_rx;
+   reg [8:0]   count_rx;
    reg [7:0]   shift_rx;
-   reg         ser_clk;
+
+   reg [3:0]   state_tx;
+   reg [8:0]   count_tx;
+   reg [7:0]   shift_tx;
  
 
      
    // Serial clock generation - 115200 baud = 50MHz/434
-   always @(posedge clk) begin
+   always @(posedge clk or negedge nRst) begin
       if(!nRst) begin
-         ser_clk  <= 0;
-         count    <= 0;
-         shift_rx <= 0;
-         busy_rx  <= 0;
+
+         // RX
+         count_rx    <= 0;
+         shift_rx    <= 0;
+         busy_rx     <= 0;
+         state_rx    <= RX_IDLE;
+
+         // TX
+         count_tx    <= 0;
+         shift_tx    <= 0;
+         busy_tx     <= 0;
+         state_tx    <= TX_IDLE;
+         tx          <= 1;
+
       end else begin
-         count <= count + 1;
-         case(state)
-            IDLE:    begin                                     // Wait for incoming
-                        count       <= 0;
-                        recieved    <= 0;
-                        if(!rx) begin
-                           state    <= START;
-                           busy_rx  <= 1; 
+
+         // RX state machine
+         count_rx <= count_rx + 1;
+         case(state_rx)
+            RX_IDLE:    begin                                     // Wait for incoming
+                           count_rx    <= 0;
+                           recieved    <= 0;
+                           if(!rx) begin
+                              state_rx <= RX_START;
+                              busy_rx  <= 1; 
+                           end
                         end
-                     end
-            START:   if(count == BAUD_05) begin                // Half sample
-                        state       <= RX_1;
-                        count       <= 0;
-                     end
-            RX_1, RX_2, RX_3, RX_4, RX_5, RX_6, RX_7:          // Shift in the bits at sample
-                     if(count == BAUD) begin
-                        shift_rx    <= {rx,shift_rx[7:1]};
-                        count       <= 0;
-                        state       <= state + 1;
-                     end
-            RX_8:    if(count == BAUD) begin                   // Last bit
-                        data_rx    <= {rx,shift_rx[7:1]};
-                        count       <= 0;
-                        state       <= WAIT;
-                        recieved    <= 1;
-                     end
-            WAIT:    if(rx) begin                              // Wait for line to go high
-                        busy_rx     <= 0;
-                        state       <= IDLE;
-                        count       <= 0;
-                     end
-            default: state <= IDLE;
+            RX_START:   if(count_rx == BAUD_05) begin             // Half sample
+                           state_rx    <= RX_1;
+                           count_rx    <= 0;
+                        end
+            RX_1, 
+            RX_2, 
+            RX_3, 
+            RX_4, 
+            RX_5, 
+            RX_6, 
+            RX_7:       if(count_rx == BAUD) begin                // Shift in bits
+                           shift_rx    <= {rx,shift_rx[7:1]};
+                           count_rx    <= 0;
+                           state_rx    <= state_rx + 1;
+                        end
+            RX_8:       if(count_rx == BAUD) begin                // Last bit
+                           data_rx     <= {rx,shift_rx[7:1]};
+                           count_rx    <= 0;
+                           state_rx    <= RX_WAIT;
+                           recieved    <= 1;
+                        end
+            RX_WAIT:    if(rx) begin                              // Wait for line to go high
+                           busy_rx     <= 0;
+                           state_rx    <= RX_IDLE;
+                           count_rx    <= 0;
+                        end
+            default:    state_rx       <= RX_IDLE;
+         endcase
+
+
+
+         // TX state machine
+         count_tx <= count_tx + 1;
+         case(state_tx)
+            TX_IDLE:    begin                                     // When told to transmit take line low
+                           count_tx    <= 0;
+                           if(transmit) begin
+                              shift_tx <= data_tx;
+                              state_tx <= TX_1;
+                              busy_tx  <= 1;
+                              tx       <= 0; 
+                           end
+                        end
+            TX_1, 
+            TX_2, 
+            TX_3, 
+            TX_4, 
+            TX_5, 
+            TX_6, 
+            TX_7,
+            TX_8:       if(count_tx == BAUD) begin                // Shift out bits
+                           tx          <= shift_tx[7];
+                           shift_tx    <= shift_tx << 1;
+                           count_tx    <= 0;
+                           state_tx    <= state_tx + 1;
+                        end
+            TX_WAIT:    if(count_tx == BAUD) begin                // Return line high and wait
+                           state_tx    <= TX_IDLE;
+                           tx          <= 1;
+                           busy_tx     <= 0;
+                        end
+            default:    state_tx       <= TX_IDLE;
          endcase
       end
    end
