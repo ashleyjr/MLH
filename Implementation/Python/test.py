0f929514fa7d4edaf8cf5c00060b41b23979c959
Ashley J. Robinson <ashley181291@gmail.com>
2015-04-23 22:21:17 +0100

    shifting working

--- Implementation/Python/test.py
+++ Implementation/Python/test.py
@@ -1,10 +1,19 @@
-import serial, time
+import serial, time, sys
 
 port = serial.Serial("/dev/ttyUSB0", baudrate=115200, timeout=0.1)
 
+check = 0
 for send in range(1,10000):
-    print "Send: " + str(255)
-    port.write(chr(255))
-    time.sleep(0.01)
-    while(port.inWaiting()):
-        print ord(port.read(1))
+    sys.stdout.write( "Send: " + str(send) + "      ")
+    port.write(chr(27))
+    check = check + 27
+    total = 0
+    loop = 0
+    for i in range(1,16):
+        data = ord(port.read(1))
+        sys.stdout.write(str(data) + "\t")
+        total = total + (data << (loop*8))
+        loop = loop + 1
+    print
+    print total
+    print check
