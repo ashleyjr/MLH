9bf25f6afdd26dba7c0d7c11516884a151bd9f8b
Ashley J. Robinson <ashley181291@gmail.com>
2015-05-05 20:16:30 +0100

    try synth

--- Implementation/Python/test1.py
+++ Implementation/Python/test1.py
@@ -2,8 +2,8 @@ import serial, time, sys
 
 port = serial.Serial("/dev/ttyUSB0", baudrate=115200, timeout=0.1)
 
-test=[  100,0,1,2,3,4,
-        100,1,10,20,30,40,
+test=[  100,0,32,0,0,0,
+        100,1,16,0,0,0,
         100,5,10,20,30,40,
         100,6,10,20,30,40,
         100,2,10,20,30,40]
@@ -12,11 +12,11 @@ port.flushInput()
 
 for i in range(0,30):
     sys.stdout.write( "Send " + str(i) + " " + str(test[i]) + "      \n\r")
-    time.sleep(0.1)
+    time.sleep(0.05)
     port.write(chr(test[i]))
 
 time.sleep(0.1)
-print port.inWaiting()
+#print port.inWaiting()
 
 total = 0
 loop = 0
